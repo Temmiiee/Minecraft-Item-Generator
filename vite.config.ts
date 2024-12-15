224c724a0094d8104c557ce2f8ccca12b9397f9d
Temmiie <mattheotermine104@gmail.com>
2024-12-15 22:53:14 +0100

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/minecraft-item-generator/',
+  base: '/src',
   plugins: [react()],
   build: {
     sourcemap: true
