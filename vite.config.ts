a2b12f8368fb872af3de10b5e82ac2146490d679
Temmiie <mattheotermine104@gmail.com>
2024-12-16 08:24:51 +0100

    modified:   index.html 	modified:   package.json 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,35 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Minecraft-Item-Generator/',
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: 'main.js',
-      },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
   },
-});
+  server: {
+    port: 3000,
+    open: true,
+    strictPort: true,
+  },
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        }
+      }
+    },
+    sourcemap: true,
+  },
+  preview: {
+    port: 3000,
+    open: true,
+  },
+})
