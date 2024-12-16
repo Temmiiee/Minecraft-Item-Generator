99b46bef023d57ab0c502aa5812079ab3316b384
Temmiie <mattheotermine104@gmail.com>
2024-12-16 08:50:37 +0100

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Minecraft-Item-Generator/',
+  base: '/minecraft-item-generator/',
   plugins: [react()],
 })
