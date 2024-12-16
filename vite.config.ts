9a8d3868748dd7a1081567111729dae4762c6835
Temmiie <mattheotermine104@gmail.com>
2024-12-16 08:30:48 +0100

    modified:   .github/workflows/deploy.yml 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,34 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Minecraft-Item-Generator/', // Nom exact de votre dépôt
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: {
-    port: 3000,
-    open: true,
-    strictPort: true,
-  },
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor'
-          }
-        }
-      }
-    },
-    sourcemap: true,
-  },
-  preview: {
-    port: 3000,
-    open: true,
   },
 })
