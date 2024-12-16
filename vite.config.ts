d1ac1e35c8895a1257cffefc246ab2ffcd2261ba
Temmiie <mattheotermine104@gmail.com>
2024-12-16 09:30:22 +0100

    modified:   index.html 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Minecraft-Item-Generator/',
+  base: '/Minecraft-Item-Generator/', // Utilisez le nom de votre dépôt GitHub
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html',
-      },
-    },
-  },
-  define: {
-    'process.env.BASE_URL': JSON.stringify('/Minecraft-Item-Generator/'),
-  },
 });
